ce520626864ce1c269f71c515b59f1a586dd208e
pfaithmtan <pfaithmtan@gmail.com>
2019-09-24 20:04:18 -0700

    Complete basic chaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,13 +8,34 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
 var Promise = require('bluebird');
-
+var fs = Promise.promisifyAll(require('fs'));
+var request = Promise.promisify(require('request'));
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return fs.readFileAsync(readFilePath)
+    .then(function(contentsOfFile) {
+      var firstLine = String(contentsOfFile).split('\n')[0];
+      if (!contentsOfFile) {
+        throw new Error('Content is empty!');
+      } else {
+        var url = 'https://api.github.com/users/' + firstLine;
+        return request(url);
+      }
+    })
+    .then(function(infoFromRequest) {
+      console.log("infoFromRequest: ", typeof infoFromRequest.body);
+      return fs.writeFileAsync(writeFilePath, infoFromRequest.body);
+    })
+    .then(function(data) {
+      console.log('Done writing!');
+      return data;
+    })
+    .catch(function(err) {
+      console.log('Hit an error: ', err.message);
+    });
 };
 
 // Export these functions so we can test them
